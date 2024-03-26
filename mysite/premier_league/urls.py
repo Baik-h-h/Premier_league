20069180232a0832818aa19e00731baf80f21f8a
bhh <bhh@example.com>
2024-03-27 01:55:11 +0900

    03/27_01

--- mysite/premier_league/urls.py
+++ mysite/premier_league/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "premier_league"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("search/", views.search, name="search"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/premier_league/views.py
+++ mysite/premier_league/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django import forms
+
+#class SearchForm(forms.Form):
+#    search = forms.CharField(label='enter player name : ', max_length=40)
 
 # Create your views here.
 def index(request):
-    context = {'example' : 1 }
-    return render(request, "premier_league/index.html", context)
+    #context = {'example' : 1 }
+    return render(request, "premier_league/index.html")
+
+def search(request):
+    search_name = request.POST.get('q')
+    context = {'search_name':search_name}
+    #여기에 이름 찾는 알고리즘 넣기
+    return render(request, "premier_league/search_result.html", context)
+
+
